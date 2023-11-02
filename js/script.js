aafc94d64b24ea4e03f9b06d70e7c541339f91fb
ronaldof16 <ronaldof16@hotmail.com>
2023-11-02 17:07:01 -0300

    Adição dos arquivos de javascript que foram feitos na pasta de execícios-js no repositório do curso

---
+++ js/script.js
@@ -0,0 +1,14 @@
+import initTabNav from "./initTab.js";
+import initAcordion from "./initAcordion.js";
+import initScrollSuave from "./initScrollSuave.js";
+import intiAnimacaoScroll from "./initAnimacao.js";
+import initModal from "./modal.js";
+import initToolTip from "./tooltip.js";
+
+
+initTabNav();
+initAcordion();
+initScrollSuave();
+intiAnimacaoScroll();
+initModal();
+initToolTip();
