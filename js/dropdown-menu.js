59812ce403eee7d0a32661f6daf73f71a59d4a9b
ronaldof16 <ronaldof16@hotmail.com>
2023-11-04 14:44:07 -0300

    criação da função outsideClick

--- js/dropdown-menu.js
+++ js/dropdown-menu.js
@@ -1,16 +1,20 @@
+import outsideClick from "./outsideclick.js";
+
 export default function initDropdownMenu() {
-    
-}
+    const dropdownMenu = document.querySelectorAll("[data-dropdown]")
 
-const dropdownMenu = document.querySelectorAll("[data-dropdown]")
-
-dropdownMenu.forEach((menu) => {
-    ["touchstart", "click"].forEach(userEvent => {
-        menu.addEventListener(userEvent, handleClick)
+    dropdownMenu.forEach((menu) => {
+        ["touchstart", "click"].forEach(userEvent => {
+            menu.addEventListener(userEvent, handleClick)
+        })
     })
-})
 
-function handleClick(event) {
-    event.preventDefault();
-    this.classList.toggle("active");
+    function handleClick(event) {
+        event.preventDefault();
+        this.classList.add("active");
+        outsideClick(this, ["touchstart", "click"], () => {
+            this.classList.remove("active")
+        })
+    }
 }
+
---
+++ js/outsideclick.js
@@ -0,0 +1,21 @@
+export default function outsideClick(element, events, callback) {
+    const html = document.documentElement;
+    const outside = "data-outside";
+
+    if(!element.hasAttribute(outside)) {
+        events.forEach(userEvent => {
+            html.addEventListener(userEvent, handlOutsideClick);
+        })
+        element.setAttribute(outside, "");
+    }
+    
+    function handlOutsideClick(event) {
+        if(!element.contains(event.target)) {
+           element.removeAttribute(outside);
+           events.forEach(userEvent => {
+            html.removeEventListener(userEvent, handlOutsideClick);
+           })
+           callback();
+        }
+    }
+}
