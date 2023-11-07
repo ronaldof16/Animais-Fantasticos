002c18b46092c21dda079622181e7ebf3f253678
ronaldof16 <ronaldof16@hotmail.com>
2023-11-07 09:59:35 -0300

    Adicionando a classe active primeiro ao menuList

--- js/menu-mobile.js
+++ js/menu-mobile.js
@@ -4,14 +4,13 @@ export default function initMenuMobile() {
     const menuButton = document.querySelector("[data-menu='button']");
     const menuList = document.querySelector("[data-menu='list']");
     const eventos = ["click", "touchstart"];
-
     
     function openMenu() {
-        menuButton.classList.add("active");
         menuList.classList.add("active");
+        menuButton.classList.add("active");
         outsideClick(menuList, eventos, () => {
-            menuButton.classList.remove("active");
             menuList.classList.remove("active");
+            menuButton.classList.remove("active");
         })
     }
 
