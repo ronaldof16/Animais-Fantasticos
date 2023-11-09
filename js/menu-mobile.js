4f81f3c3472d11cd004d922415b59c6d9f3a8e30
ronaldof16 <ronaldof16@hotmail.com>
2023-11-09 08:59:14 -0300

    Exclusão do evento de touchstart

--- js/menu-mobile.js
+++ js/menu-mobile.js
@@ -3,20 +3,22 @@ import outsideClick from "./outsideclick.js";
 export default function initMenuMobile() {   
     const menuButton = document.querySelector("[data-menu='button']");
     const menuList = document.querySelector("[data-menu='list']");
-    const eventos = ["click", "touchstart"];
+    //const eventos = ["click", "touchstart"];
     
     function openMenu() {
         menuList.classList.add("active");
         menuButton.classList.add("active");
-        outsideClick(menuList, eventos, () => {
+        outsideClick(menuList, ["click"], () => {
             menuList.classList.remove("active");
             menuButton.classList.remove("active");
         });
     };
 
-    if(menuButton) {
+    menuButton.addEventListener("click", openMenu);
+
+    /*if(menuButton) {
         eventos.forEach(evento => {
             menuButton.addEventListener(evento, openMenu);
         });
-    };
+    };*/
 };
